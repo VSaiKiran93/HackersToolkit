208d7d39c6d5a8e3adcef6402b4b54c443094cc4
VSaiKiran93 <venkatsaikiran.kureti@yahoo.com>
2023-04-02 23:32:57 -0500

    Updated code

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from django.views.generic.base import RedirectView
 from .views import NmapScanView
+
+#favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True),
 
 urlpatterns =[
      #path('command_output/<str:target_ip>/', views.execute_command),
-     path('api/nmap-scan/', NmapScanView.as_view(), name='nmap-scan-api'),
+     # path('nmap-scan', NmapScanView.as_view()),
+     path('', NmapScanView.as_view(), name='nmap-scan-api'),
+     #path('favicon.ico', favicon_view),
 ]
