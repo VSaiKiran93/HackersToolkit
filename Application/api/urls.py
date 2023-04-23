3469e370a78b5e6db351770480e97f555101aef5
Venkta Sai Kiran <82889124+VSaiKiran93@users.noreply.github.com>
2023-04-23 16:02:37 -0500

    updated urls

--- Application/api/urls.py
+++ Application/api/urls.py
@@ -1,11 +1,12 @@
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 from django.views.generic.base import RedirectView
-from .views import NmapScanView, nikto_scan
+from .views import NmapScanView, scan_view
 
 urlpatterns =[
      path('', NmapScanView.as_view(), name='nmap'),
-     path('nikto/', nikto_scan, name='nikto'),
+     path('openvas/', scan_view, name='openvas'),
+     #path('nikto/', nikto_scan, name='nikto'),
      #path('openvas-scan/<str:ip_address>/', vulnerability_scan, name='vulnerability_scan'),
 ]
 
